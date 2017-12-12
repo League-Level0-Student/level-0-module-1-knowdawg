3ef097b1780018a69640fe69eb3b22853efc9ba4
67tasman <league@wts14.attlocal.net>
2017-12-11 19:59:23 -0800

    Xgeeks

--- src/XGeeks.java
+++ src/XGeeks.java
@@ -1,8 +1,6 @@
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
-
-
-
 
 import javax.swing.JOptionPane;
 
@@ -12,14 +10,36 @@ import javax.swing.JOptionPane;
 * your program should say "June's superpower is writing recipes".
 */
 public class XGeeks {
-public static void main(String[] args) {
+	public static void main(String[] args) {
 
 		// 1. Save the superpower for each person in a variable.
-
+		String MattsSuperpower = "Talking with cats";
+		String NolansSuperpower = "Fat minipulation";
+		String MaClarynsPower = "flying";
+		String JensensPower = "turning into a fox";
+		String DaelynsPower = "crustean mimicry";
 		// 2. Ask the user to enter a name. Store their answer in a variable.
+		String name = JOptionPane.showInputDialog("What is your name?");
+		// 3. Show the superpower in a pop-up, depending on the name entered.
+		if (name.equals("Matt")) {
+			JOptionPane.showMessageDialog(null, "Your super power is " + MattsSuperpower);
+		} else {
+			if (name.equals("Nolan")) {
+				JOptionPane.showMessageDialog(null, "Your super power is " + NolansSuperpower);
+			} else {
+				if (name.equals("MaClaryn")) {
+					JOptionPane.showMessageDialog(null, "Your super power is " + MaClarynsPower);
+				} else if (name.equals("Jensen")) {
+					JOptionPane.showMessageDialog(null, "Your super power is " + JensensPower);
+				} else {
+					if (name.equals("Daelyn")) {
+						JOptionPane.showMessageDialog(null, "Your super power is " + DaelynsPower);
+					} else {
 
-		// 3. Show the superpower in a pop-up, depending on the name entered. 
-
+						JOptionPane.showMessageDialog(null, "Your super power is NOTHING");
+					}
+				}
+			}
+		}
 	}
 }
-
